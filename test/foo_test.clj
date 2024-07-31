8dd137a95ad175a49c65d8ba25dc28f935f017fc
Imre Kószó <imrekoszo@users.noreply.github.com>
2024-07-31 12:23:55 +0200

    add more test cases

--- test/foo_test.clj
+++ test/foo_test.clj
@@ -3,19 +3,106 @@
    [clojure.template :refer [do-template]]
    [clojure.test :refer :all]))
 
-(deftest foo-test
-  (do-template
-    [param expected]
-    (testing (str "case 1: " param)
-      (is (= expected {})))
+;; It appears this is related to a comment in the first do-template
+;; while having multiple cases for the second do-template???
 
-    false
+(deftest wrong-1
+  (do-template [expected]
+    (testing "case 1"
+      (is (not= expected expected)))
+
     {:bar 1 #_2})
 
-  (do-template
-    [param]
-    (testing (str "case 2: " param)
-      (is (= 1 1)))
+  (do-template [expected]
+    (testing "case 2"
+      (is (= expected expected)))
 
     :foo
     :bar))
+
+(deftest wrong-2
+  (do-template [expected]
+    (testing "case 1"
+      (is (not= expected expected)))
+
+    {:bar 1} ;
+    )
+
+  (do-template [expected]
+    (testing "case 2"
+      (is (= expected expected)))
+
+    :foo
+    :bar))
+
+(deftest wrong-3
+  (do-template [expected]
+    (testing "case 1"
+      (is (not= expected expected)))
+
+    {:bar 1}
+    ;
+    )
+
+  (do-template [expected]
+    (testing "case 2"
+      (is (= expected expected)))
+
+    :foo
+    :bar))
+
+(deftest good-1
+  (do-template [expected]
+    (testing "case 1"
+      (is (not= expected expected)))
+
+    {:bar 1})
+
+  (do-template [expected]
+    (testing "case 2"
+      (is (= expected expected)))
+
+    :foo
+    :bar))
+
+(deftest good-2
+  (do-template [expected]
+    (testing "case 1"
+      (is (not= expected expected)))
+
+    {:bar 1 #_2})
+
+  (do-template [expected]
+    (testing "case 2"
+      (is (= expected expected)))
+
+    :foo))
+
+(deftest good-3
+  (do-template [expected]
+    (testing "case 1"
+      (is (not= expected expected)))
+
+    {:bar 1}
+    ;
+    )
+
+  (do-template [expected]
+    (testing "case 2"
+      (is (= expected expected)))
+
+    :foo))
+
+(deftest good-4
+  (do-template [expected]
+    (testing "case 1"
+      (is (not= expected expected)))
+
+    ;
+    {:bar 1})
+
+  (do-template [expected]
+    (testing "case 2"
+      (is (= expected expected)))
+
+    :foo))
